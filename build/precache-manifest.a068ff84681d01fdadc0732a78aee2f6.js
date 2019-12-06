2c2eb405e33d9f95b32cc04979d658c210574772
András SZUCSÁK <andras.szucsak@nng.com>
2019-12-06 15:51:12 +0100

    initial commit

---
+++ build/precache-manifest.a068ff84681d01fdadc0732a78aee2f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67427c345fdcd409f151c9ed609e1d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edc147da1c3da1f5966",
+    "url": "/static/css/main.641563f1.chunk.css"
+  },
+  {
+    "revision": "0ddc0836c8dc09655c7a",
+    "url": "/static/js/2.82671af4.chunk.js"
+  },
+  {
+    "revision": "2edc147da1c3da1f5966",
+    "url": "/static/js/main.39271c27.chunk.js"
+  },
+  {
+    "revision": "e7b61e672cd36add5dc7",
+    "url": "/static/js/runtime-main.a95ca455.js"
+  },
+  {
+    "revision": "1ed6a2afbf41449be5264351011e7a1b",
+    "url": "/static/media/default.1ed6a2af.svg"
+  }
+]);
